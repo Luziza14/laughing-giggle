5d16e7878e1d854bed7825c24be057dd121a4268
Fernanda Luiza <141834497+Luziza14@users.noreply.github.com>
2024-05-07 16:28:26 -0300

    Create Estudos.php

---
+++ Estudos.php
@@ -0,0 +1,27 @@
+<?php
+
+//variável
+$numero = 3;
+$real = 23.9;
+$boolean = true;
+$texto = "Ariba";
+
+//teste 
+$idade = 21;
+$curso = "ADS";
+$status = true;
+
+$conta = $umero * $idade;
+
+echo "O número que saiu da conta foi " . $conta . "<br>"; 
+echo "O curso é " . $curso . " E seu status é " . $status;
+
+$num = $_GET['num'];
+echo "numero digitado" . $num;
+
+
+//print || mandar o que estiver dentro, ainda não entendi mutio bem
+echo "Olá Mundo". $numero ;
+
+//
+?>
